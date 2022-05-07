133bfa747f991f82e892ea079a3d2c509fabdc2d
daominhkhanh20 <khanhc1k36@gmail.com>
2022-05-07 21:27:17 +0700

    update bash script

--- scripts/scrapy_crawl.sh
+++ scripts/scrapy_crawl.sh
@@ -1,5 +1,9 @@
+# get url file from web
 python3 src/utils/make_url_cellphones.py
 python3 src/utls/make_url_tgdd.py
 cd Crawl
+
+# crawl cellphones
 scrapy crawl cellphones
+#crawl the gioi di dong
 scrapy crawl tgdd
