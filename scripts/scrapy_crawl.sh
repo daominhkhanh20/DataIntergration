5a8ea91555d27ce49f4948f561c41c8703769174
vukhanh09 <vukhanh0920@gmail.com>
2022-05-07 21:54:09 +0700

    add

--- scripts/scrapy_crawl.sh
+++ scripts/scrapy_crawl.sh
@@ -3,7 +3,13 @@ python3 src/utils/make_url_cellphones.py
 python3 src/utls/make_url_tgdd.py
 cd Crawl/Scrapy
 
-# crawl cellphones
+crawl cellphones
 scrapy crawl cellphones
 #crawl the gioi di dong
 scrapy crawl tgdd
+
+scrapy crawl phongvu -O ../Url/phongvu/phongvu_phone_url.json -a device=phone
+scrapy crawl phongvu -O ../Url/phongvu/phongvu_laptop_url.json -a device=laptop
+
+cd ../api
+python phongvu_extract.py
